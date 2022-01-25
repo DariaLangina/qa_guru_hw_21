d78365e4d7363bbd14af07bbc0ed9a8927c38fa6
DariaLangina <dlangina@rvision.ru>
2022-01-25 11:19:38 +0300

    added @ExtendWith({AllureJunit5.class})

--- src/test/java/dlangina/tests/TestBase.java
+++ src/test/java/dlangina/tests/TestBase.java
@@ -8,11 +8,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import dlangina.drivers.BrowserstackMobileDriver;
 import dlangina.helpers.Attach;
+import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith({AllureJunit5.class})
 public class TestBase {
 
   @BeforeAll
